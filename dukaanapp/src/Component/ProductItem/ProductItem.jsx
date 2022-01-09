c35325ebeec9bab49a442cfa70c86c83fe65d08a
Meghraj <meghraj.deshmukh103@gmail.com>
2022-01-09 13:24:09 +0530

    onClick event modified with event bubbling principal

--- dukaanapp/src/Component/ProductItem/ProductItem.jsx
+++ dukaanapp/src/Component/ProductItem/ProductItem.jsx
@@ -2,33 +2,39 @@ import React ,{useContext} from 'react'
 import styles from './styles.module.css';
 import { State } from '../../Context/GlobalState';
 import { Routes, Link, Route, useParams, useNavigate } from "react-router-dom"
-
+const ADD_BTN="ADD_BTN";
+const PRODUCT_CLICK="PRODUCT_CLICK";
 
 const ProductItem =({product}) => {
     const {addItemToCart}=useContext(State);
     const navigate=useNavigate();
     
 
-    function onClickProduct(){
-        console.log("prodct",product);
-        navigate('/product',{state:product});
+    function onClickEvent(event){
+         event.preventDefault();
+        const clickId=event.target.id;
+        if(clickId===ADD_BTN){
+        addItemToCart((pre)=>[...pre,Math.random()]);
+        }else{
+            console.log("prodct",console.log("event",event));
+            navigate('/product',{state:product}); 
+        }
+        
 
     }
     return (
-        <div className={styles.productCard} onClick={onClickProduct}>
+        <div className={styles.productCard} id={PRODUCT_CLICK} onClick={onClickEvent}>
         <div className={styles.productImage}>
             <img src={product.image}></img>
         </div>  
-        <div className={styles.productDetail}>
+        <div className={styles.productDetail} >
            <div className={styles.productName}>{product.name}</div>
             <small>{product.base_qty}</small>
          <div className={styles.productPrice}>
              <div className={styles.price}>
             <div >{product.original_cost}</div>
             </div>
-        <div className={styles.AddProduct}><button onClick={()=>{
-            addItemToCart((pre)=>[...pre,Math.random()]);
-        }}>Add +</button></div>
+        <div className={styles.AddProduct}><button id={ADD_BTN}>Add +</button></div>
          </div>
          </div>
       </div>
